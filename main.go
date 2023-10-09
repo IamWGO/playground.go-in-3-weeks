a2e408639e898a6011b928c4bd951e464eb09740
iamWGO <gottlieb@gmail.com>
2023-10-09 14:13:25 +0200

    goto Statment

--- main.go
+++ main.go
@@ -3,21 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	dividend, divisor := 10, 5
-	fmt.Printf("%v divided by %v is %v\n", dividend, divisor, divide(dividend, divisor))
 
-	fmt.Println(" -- --- -- ")
-
-	dividend, divisor = 10, 0
-	fmt.Printf("%v divided by %v is %v\n", dividend, divisor, divide(dividend, divisor))
-	// Panic will print because 10/0 is not accepted
+i := 10
+if i<15 {
+	fmt.Println(i)
+	goto myLabel
 }
 
-func divide(dividend, divisor int) int {
-	defer func() {
-		if recover() != nil {
-			fmt.Println("Panic detected!")
-		}
-	}()
-	return dividend / divisor
-}
+myLabel : 
+	j := 42
+	for i := 0; i < j; i++ {
+		fmt.Println(i)
+	}
+ 
+} 
