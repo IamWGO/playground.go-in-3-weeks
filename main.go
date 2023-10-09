d5d0e6979a09c8efd07aa77cc560310626a4e9b6
iamWGO <gottlieb@gmail.com>
2023-10-09 13:52:38 +0200

    Basic if statment

--- main.go
+++ main.go
@@ -1,42 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 )
 
 func main() {
-	fmt.Println("Please select an option")
-	fmt.Println("1) Print menu")
-	in := bufio.NewReader(os.Stdin)
-	choice, _ := in.ReadString('\n')
-	choice = strings.TrimSpace(choice) // we don't know what to do with this yet!
-
-	type menuItem struct {
-		name   string
-		prices map[string]float64
-	}
-
-	if choice =="1" {
-		menu := []menuItem{
-			{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
-			{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
-		}
 	
-		for _, item := range menu {  // get item
-			fmt.Println(item.name)
-			fmt.Println(strings.Repeat("-", 10))
-			for size, cost := range item.prices {  // map [string]float64  = size, cost
-				fmt.Printf("\t%10s%10.2f\n", size, cost)
-			}
-		}
-		return
-	} 
-
-
-	fmt.Println("Wrong command")
+	if i := 5; i < 5 {
+		fmt.Println("i is less than 5")
+	} else if i < 10 {
+		fmt.Println("i is less than 10")
+	} else {
+		fmt.Println("i is greater than or equal to 10")
+	}
 	
 
 }
