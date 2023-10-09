a19468f3634e05a645fc4d9e527e08dbea2cc03c
iamWGO <gottlieb@gmail.com>
2023-10-09 13:57:46 +0200

    Switch case + input command (menu)

--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	
-	if i := 5; i < 5 {
-		fmt.Println("i is less than 5")
-	} else if i < 10 {
-		fmt.Println("i is less than 10")
-	} else {
-		fmt.Println("i is greater than or equal to 10")
+
+	type menuItem struct {
+		name   string
+		prices map[string]float64
 	}
-	
+
+	menu := []menuItem{
+		{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
+		{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
+	}
+
+	in := bufio.NewReader(os.Stdin)
+
+loop:
+	for {
+		fmt.Println("Please select an option")
+		fmt.Println("1) Print menu")
+		fmt.Println("2) Add item")
+		fmt.Println("q) quit")
+		choice, _ := in.ReadString('\n')
+
+		switch strings.ToLower(strings.TrimSpace(choice)) { //trim space and to lowercase
+		case "1":
+			for _, item := range menu {
+				fmt.Println(item.name)
+				fmt.Println(strings.Repeat("-", 10))
+				for size, cost := range item.prices {
+					fmt.Printf("\t%10s%10.2f\n", size, cost)
+				}
+			}
+		case "2":
+			fmt.Println("Please enter the name of the new item")
+			name, _ := in.ReadString('\n')
+			menu = append(menu, menuItem{name: strings.TrimSpace(name), prices: make(map[string]float64)}) // add new MenuItem with empty prices
+		case "q":
+			break loop  //quit loop
+		default:
+			fmt.Println("Unknown option")
+		}
+	}
 
 }
