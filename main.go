63d41c86de6471309d63f8dddb5df9f8d42a5e6b
iamWGO <gottlieb@gmail.com>
2023-10-09 13:50:42 +0200

    Collection loop and check input command

--- main.go
+++ main.go
@@ -19,17 +19,24 @@ func main() {
 		prices map[string]float64
 	}
 
-	menu := []menuItem{
-		{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
-		{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
-	}
-
-	for _, item := range menu {
-		fmt.Println(item.name)
-		fmt.Println(strings.Repeat("-", 10))
-		for size, cost := range item.prices {
-			fmt.Printf("\t%10s%10.2f\n", size, cost)
+	if choice =="1" {
+		menu := []menuItem{
+			{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
+			{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
 		}
-	}
+	
+		for _, item := range menu {  // get item
+			fmt.Println(item.name)
+			fmt.Println(strings.Repeat("-", 10))
+			for size, cost := range item.prices {  // map [string]float64  = size, cost
+				fmt.Printf("\t%10s%10.2f\n", size, cost)
+			}
+		}
+		return
+	} 
+
+
+	fmt.Println("Wrong command")
+	
 
 }
