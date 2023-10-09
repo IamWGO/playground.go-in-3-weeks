57972524d4cca90604d16b94c17e1f57f705a4d7
iamWGO <gottlieb@gmail.com>
2023-10-09 17:08:15 +0200

    Interface

--- main.go
+++ main.go
@@ -1,34 +1,64 @@
 package main
 
 import (
-	"bufio"
-	"demo/menu"
+	"bytes"
 	"fmt"
-	"os"
 	"strings"
 )
 
-var in = bufio.NewReader(os.Stdin)
+type printer interface {
+	Print() string
+}
+
+type user struct {
+	username string
+	id       int
+}
+
+func (u user) Print() string {
+	return fmt.Sprintf("%v [%v]\n", u.username, u.id)
+}
+
+type menuItem struct {
+	name   string
+	prices map[string]float64
+}
+
+func (mi menuItem) Print() string {
+	var b bytes.Buffer
+	b.WriteString(mi.name + "\n")
+	b.WriteString(strings.Repeat("-", 10) + "\n")
+	for size, cost := range mi.prices {
+		fmt.Fprintf(&b, "\t%10s%10.2f\n", size, cost)
+	}
+
+	return b.String()
+}
 
 func main() {
+	var p printer
+	p = user{username: "adent", id: 42}
+	fmt.Println(p.Print())
 
-loop:
-	for {
-		fmt.Println("Please select an option")
-		fmt.Println("1) Print menu")
-		fmt.Println("2) Add item")
-		fmt.Println("q) quit")
-		choice, _ := in.ReadString('\n')
+	p = menuItem{name: "Coffee",
+		prices: map[string]float64{"small": 1.65,
+			"medium": 1.80,
+			"large":  1.95,
+		},
+	}
+	fmt.Println(p.Print())
 
-		switch strings.TrimSpace(choice) {
-		case "1":
-			menu.Print()
-		case "2":
-			menu.Add()
-		case "q":
-			break loop
-		default:
-			fmt.Println("Unknown option")
-		}
+	u, ok := p.(user)
+	fmt.Println(u, ok)
+	mi, ok := p.(menuItem)
+	fmt.Println(mi, ok)
+
+	switch v := p.(type) {
+	case user:
+		fmt.Println("Found a user!", v)
+	case menuItem:
+		fmt.Println("Found a menuItem!", v)
+	default:
+		fmt.Println("I'm not sure what this is...")
 	}
 }
--- menu/data.go
+++
@@ -1,6 +0,0 @@
-package menu
-
-var data = menu{
-	{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
-	{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
-}
