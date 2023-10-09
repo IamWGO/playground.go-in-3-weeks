18d47b4835c52c60c7a0130ad769a46aa6bb5b14
iamWGO <gottlieb@gmail.com>
2023-10-09 13:05:46 +0200

    Collection loop

--- main.go
+++ main.go
@@ -1,26 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	i := 99
+	fmt.Println("Please select an option")
+	fmt.Println("1) Print menu")
+	in := bufio.NewReader(os.Stdin)
+	choice, _ := in.ReadString('\n')
+	choice = strings.TrimSpace(choice) // we don't know what to do with this yet!
 
-	for {
-		fmt.Println(i)
-		i += 1
-		break
+	type menuItem struct {
+		name   string
+		prices map[string]float64
 	}
 
-	i = 5
-	for i < 10 {
-		fmt.Println(i)
-		i++
+	menu := []menuItem{
+		{name: "Coffee", prices: map[string]float64{"small": 1.65, "medium": 1.80, "large": 1.95}},
+		{name: "Espresso", prices: map[string]float64{"single": 1.90, "double": 2.25, "triple": 2.55}},
 	}
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	for _, item := range menu {
+		fmt.Println(item.name)
+		fmt.Println(strings.Repeat("-", 10))
+		for size, cost := range item.prices {
+			fmt.Printf("\t%10s%10.2f\n", size, cost)
+		}
 	}
 
 }
